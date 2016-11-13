1cb1d1b335769957e960ee79f8c7d601e8fef25b
Austin Bonander <austin.bonander@gmail.com>
2016-11-13 07:00:36 -0800

    Document possible compiler issues in `test_service`

--- examples/test_service.rs
+++ examples/test_service.rs
@@ -1,12 +1,13 @@
-//N.B.: this requires nightly to build because of the `proc_macro` feature. However,
-//this is only necessary for the sake of brevity: on the stable and beta channels,
-// you can use `serde_codegen` and a build script to generate this implementation
+// N.B.: this requires nightly to build because of the `proc_macro` feature. However,
+// this is only necessary for the sake of brevity: on the stable and beta channels,
+// you can use `serde_codegen` and a build script to generate a `Deserialize` impl
 // as described here: https://serde.rs/codegen-stable.html.
 #![feature(proc_macro)]
 
 #[macro_use]
 extern crate anterofit;
 
+// If you get a "not found" error here, enable the `nightly` feature (nightly channel required)
 #[macro_use]
 extern crate serde_derive;
 
@@ -41,6 +42,7 @@ fn main() {
 
     let adapter = Adapter::builder()
         .base_url(url)
+        // If you get a "not found" error from the compiler here, enable the `json` feature.
         .deserialize(anterofit::serialize::json::Deserializer)
         .build();
 
